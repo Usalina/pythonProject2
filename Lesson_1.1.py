6cecf275389c5bffe60fe1d6087e6f6d016eb95a
Usalina <usalina9@mail.ru>
2021-09-27 22:41:21 +0900

    Initial commit

---
+++ Lesson_1.1.py
@@ -0,0 +1,15 @@
+# Посмотреть документацию к API GitHub, разобраться как вывести список репозиториев
+# для конкретного пользователя, сохранить JSON-вывод в файле *.json.
+
+# документацию можно посмотреть здесь https://docs.github.com/en/rest
+
+import requests
+
+username = 'Usalina'
+token = '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'
+r = requests.get('https://api.github.com/user/repos', auth=(username, token))
+r.json()
+f = open("requests.json", "w")
+f.write(r)
+f.close()
+
